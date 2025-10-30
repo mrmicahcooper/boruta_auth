37e22c18c3840e7b936d5c68660d133a9728a3c1
Micah Cooper <micah.cooper@mimic.com>
2025-10-30 08:12:51 -0500

    Don't use md5

--- lib/boruta/oauth/schemas/client.ex
+++ lib/boruta/oauth/schemas/client.ex
@@ -244,7 +244,7 @@ defmodule Boruta.Oauth.Client do
 
     @spec kid_from_private_key(private_pem :: String.t()) :: kid :: String.t()
     def kid_from_private_key(private_pem) do
-      :crypto.hash(:md5, private_pem) |> Base.encode64() |> String.slice(0..16)
+      :crypto.hash(:sha256, private_pem) |> Base.encode64() |> String.slice(0..16)
     end
 
     @spec userinfo_signature_type(Client.t()) :: id_token_signature_type :: atom()
